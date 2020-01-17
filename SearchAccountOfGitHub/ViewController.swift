18f5bc24c9b9f93c6cfd853e0f7bd23848bdccb3
塩見陵介 <ryosukesukairain1@gmail.com>
2020-01-17 23:39:50 +0900

    PJT作成, cocoapodsの導入

---
+++ SearchAccountOfGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchAccountOfGitHub
+//
+//  Created by 塩見陵介 on 2020/01/17.
+//  Copyright © 2020 つまようじ職人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
